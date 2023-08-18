fef9a46020f656f98dad989456542ff1cabd37a7
JoseMilans <josemilans@gmail.com>
2023-08-18 20:10:52 +0000

    Fixed: memory leaks, return exit status, error handling, relative paths handling and memory leaks.

--- execute.c
+++ execute.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 /**
- * exe_cmd_with_path - Executes a command given its full path.
- * @f_path: The full path of the command.
- * @toks: The command and its arguments.
- * @prog: Program's name (argv[0]).
- * @l_num: Line counter.
- * Return: 0 if valid, -1 if not.
+ * exe_cmd_with_path - Executes a command given its full path
+ * @f_path: The full path of the command
+ * @toks: The command and its arguments
+ * @prog: Program's name (argv[0])
+ * @l_num: Line counter
+ * Return: 0 if valid, -1 if not
  */
 int exe_cmd_with_path(char *f_path, char **toks, char *prog, int l_num)
 	{
@@ -16,7 +16,7 @@ int exe_cmd_with_path(char *f_path, char **toks, char *prog, int l_num)
 	{
 		fprintf(stderr, "%s: %d: %s: %s\n", prog, l_num,
 				f_path, strerror(errno));
-		return (-1);
+		return (2);
 	}
 	pid = fork();
 	if (pid == 0) /* Child */
@@ -25,76 +25,88 @@ int exe_cmd_with_path(char *f_path, char **toks, char *prog, int l_num)
 		{
 			fprintf(stderr, "%s: %d: %s: %s\n", prog, l_num,
 					f_path, strerror(errno));
-			exit(EXIT_FAILURE);
+			exit(2);
 		}
 	}
 	else if (pid < 0) /* If fork() fails */
 	{
 		perror(prog);
-		return (-1);
+		return (2);
 	}
 	else
 	{
 		do
 			waitpid(pid, &status, WUNTRACED);
 		while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		if (WIFEXITED(status))
+			return (WEXITSTATUS(status));
 	}
 	return (0);
 }
 /**
- * direct_execute - Executes the command if it starts with '/' or '.'.
- * @tokens: The command and its arguments.
- * @prog: Program's name (argv[0]).
- * @l_num: Line counter.
+ * direct_execute - Executes the command if it starts with '/' or '.'
+ * @tokens: The command and its arguments
+ * @prog: Program's name (argv[0])
+ * @l_num: Line counter
+ * Return: Status of command execution
  */
-void direct_execute(char **tokens, char *prog, int l_num)
+int direct_execute(char **tokens, char *prog, int l_num)
+{
+	if (tokens[0][0] == '/' ||
+		tokens[0][0] == '.' ||
+		strncmp(tokens[0], "./", 2) == 0 ||
+		strncmp(tokens[0], "../", 3) == 0 ||
+		strncmp(tokens[0], "../../", 4) == 0)
 	{
-	if (exe_cmd_with_path(tokens[0], tokens, prog, l_num) == -1)
-		fprintf(stderr, "%s: %d: fork failed\n", prog, l_num);
+		return (exe_cmd_with_path(tokens[0], tokens, prog, l_num));
+	}
+	return (127);
 }
 /**
- * path_execute - Searches for cmd in PATH and executes it.
- * @tokens: The command and its arguments.
- * @prog: Program's name (argv[0]).
- * @l_num: Line counter.
+ * path_execute - Searches for cmd in PATH and executes it
+ * @tokens: The command and its arguments
+ * @prog: Program's name (argv[0])
+ * @l_num: Line counter
+ * Return: Status of command execution, -1 if error or command not found
  */
-void path_execute(char **tokens, char *prog, int l_num)
-	{
+int path_execute(char **tokens, char *prog, int l_num)
+{
 	path_t *path_list = parse_path();
-	char *full_path = search_path(tokens[0], path_list);
+	char *f_path;
+	int status;
 
 	if (!path_list)
 	{
-		fprintf(stderr, "%s: %d: failed to parse PATH\n", prog, l_num);
-		return;
-	}
-	if (full_path)
-	{
-		free(tokens[0]);  /* free the old command */
-		tokens[0] = full_path;  /* replace with full path */
-
-		if (exe_cmd_with_path(full_path, tokens, prog, l_num) == -1)
+		if (access(tokens[0], X_OK) == -1)
 		{
-			free_path_list(path_list);
-			return;
+			fprintf(stderr, "./hsh: %d: %s: not found\n", l_num,
+			tokens[0]);
+			return (127);
 		}
+		return (exe_cmd_with_path(tokens[0], tokens, prog, l_num));
 	}
-	else
-	{
-		fprintf(stderr, "%s: %d: %s: command not found\n", prog,
-				l_num, tokens[0]);
-	}
+	f_path = search_path(tokens[0], path_list);
 	free_path_list(path_list);
+	if (!f_path)
+	{
+		fprintf(stderr, "./hsh: %d: %s: not found\n", l_num, tokens[0]);
+		return (127);
+	}
+	status = exe_cmd_with_path(f_path, tokens, prog, l_num);
+	free(f_path);
+	return (status);
 }
 /**
  * execute_command - Decides the method of command execution
  * @toks: Array of command and its arguments
  * @prog: Program's name (argv[0])
  * @l_num: Line counter
+ * Return: Exit status of the command
  */
-void execute_command(char **toks, char *prog, int l_num)
+int execute_command(char **toks, char *prog, int l_num)
 {
 	int i;
+	int exit_status = 0;
 
 	if (toks[0] && strcmp(toks[0], "exit") == 0)
 	{ /* Cleanup and exit */
@@ -103,9 +115,9 @@ void execute_command(char **toks, char *prog, int l_num)
 		free(toks);
 		exit(EXIT_SUCCESS);
 	}
-	/* If the command starts with '/' or '.', just execute directly */
-	if (toks[0][0] == '/' || (toks[0][0] == '.' && toks[0][1] == '/'))
-		direct_execute(toks, prog, l_num);
-	else /* Otherwise, search for it in the PATH */
-		path_execute(toks, prog, l_num);
+	exit_status = direct_execute(toks, prog, l_num);
+
+	if (exit_status == 127)
+		exit_status = path_execute(toks, prog, l_num);
+	return (exit_status);
 }
