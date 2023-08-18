2c9c118e58063159e3c88043901304dd38827eff
JoseMilans <josemilans@gmail.com>
2023-08-18 19:41:50 +0000

    Added: functions to fix bugs and to reorginise the files. Amended: type functions due error message and status.

--- shell.h
+++ shell.h
@@ -27,16 +27,18 @@ typedef struct path_s
 } path_t;
 
 char **parse_input(char *input);
-void execute_command(char **tokens, char *prog, int l_num);
 char *search_path(char *cmd, path_t *path_list);
 char *_getenv(const char *name);
 path_t *parse_path(void);
+path_t *create_new_node(char *token);
+void append_to_list(path_t **head, path_t *new_node);
 void free_path_list(path_t *head);
-void direct_execute(char **tokens, char *prog, int l_num);
-void path_execute(char **tokens, char *prog, int l_num);
+int execute_command(char **toks, char *prog, int l_num);
+int exe_cmd_with_path(char *f_path, char **toks, char *prog, int l_num);
+int direct_execute(char **tokens, char *prog, int l_num);
+int path_execute(char **tokens, char *prog, int l_num);
 char **init_token(void);
 void check_allocation(char **tokens);
 char **resize_token_buffer(char **tokens, size_t *bufsize);
-int exe_cmd_with_path(char *f_path, char **toks, char *prog, int l_num);
 
 #endif
