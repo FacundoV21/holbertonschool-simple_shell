7e603e274965e1adb1f88334d39e103bcf34f468
JoseMilans <josemilans@gmail.com>
2023-08-21 23:30:29 +0000

    Added: new functions due the betty coding style constraints. Fixed: tokens memory is freed

--- main.c
+++ main.c
@@ -20,8 +20,8 @@ int main(int argc, char **argv)
 	char *input_line = NULL;/* Ptr to hold user's input line */
 	size_t len = 0; /* Size of the input buffer */
 	ssize_t read; /* Number of characters read by getline */
-	char **tokens;/*2D arr to hold each parsed tok frm inplin*/
-	int i, fd_isatty, line_counter = 0, exit_status = EXIT_SUCCESS;
+	char **tokens;/*2D arr to hold each parsed tok from input line*/
+	int fd_isatty, line_counter = 0, exit_status = EXIT_SUCCESS;
 	(void) argc;
 
 	signal(SIGINT, handle_sigint);
@@ -33,24 +33,41 @@ int main(int argc, char **argv)
 			printf("#cisfun$ ");  /* Display prompt */
 		read = getline(&input_line, &len, stdin);/*Read user inp. Dynallocmem*/
 		if (read == -1) /* If EOF (ctrl+d) */
-		{
-			free(input_line);
-			if (fd_isatty)
-				printf("\n");
-			return (exit_status);
-		}
-		tokens = parse_input(input_line); /* Parse inpline into tokens */
-		if (tokens[0] == NULL)
+			break;
+		tokens = parse_input(input_line); /* Parse input line into tokens */
+		if (!tokens || !tokens[0])
 		{
 			free(tokens);
 			continue;
 		}
 		exit_status = execute_command(tokens, argv[0], line_counter);
-		for (i = 0; tokens[i]; i++) /* To avoid memory leaks */
-			free(tokens[i]);
-		free(tokens); /* Main tokens ptr */
-		tokens = NULL;
-		}
-	free(input_line);/*To release mem if infloop ever breaks in fture mods*/
-	return (exit_status);/*This might not be reached due to infloop*/
+		free_tokens(tokens); /* Main tokens ptr */
+	}
+	/*To release mem if infloop ever breaks in fture mods*/
+	return (cleanup(input_line, exit_status, fd_isatty));
+}
+/**
+ * cleanup - Frees memory and handles end of program cleanup.
+ * @input_line: The user input line.
+ * @status: The exit status to return.
+ * @fd_isatty: Boolean indicating if input is from a terminal.
+ */
+int cleanup(char *input_line, int status, int fd_isatty)
+{
+	free(input_line);
+	if (fd_isatty)
+		printf("\n");
+	return (status);
+}
+/**
+ * free_tokens - Frees the tokens allocated in parse_input.
+ * @tokens: The array of token strings.
+ */
+void free_tokens(char **tokens)
+{
+	int i;
+
+	for (i = 0; tokens[i]; i++)
+		free(tokens[i]);
+	free(tokens);
 }
